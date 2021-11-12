6b6821c561b2244d4693359ae5ac87ca93ec9090
Matt Closson <mattwclosson@gmail.com>
2021-11-12 11:50:52 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,84 +2,87 @@
 // Dependencies
 /////////////////////////////////
 // get .env variables
-require("dotenv").config()
+require("dotenv").config();
 // pull PORT from .env, give it a default of 3000 (object destructuring)
-const {PORT = 3001, DATABASE_URL} = process.env
+const { PORT = 3001, DATABASE_URL } = process.env;
 // import express
-const express = require("express")
+const express = require("express");
 // create the application object
-const app = express()
+const app = express();
 // import mongoose
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 // import middleware
-const cors = require("cors")
-const morgan = require("morgan")
-
-//////////////////////////////
-// Models
-//////////////////////////////
-// the people schema
-const PeopleSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    title: String
-}, {timestamps: true})
-
-const People = mongoose.model("People", PeopleSchema)
-
-/////////////////////////////////
-//Middleware
-//////////////////////////////////
-app.use(cors()) // prevent cors errors, opens up access for frontend
-app.use(morgan("dev")) //logging
-app.use(express.json()) // parse json bodies
+const cors = require("cors");
+const morgan = require("morgan");
 
 /////////////////////////////////
 // Database Connection
 ////////////////////////////////
 // establish connection
 mongoose.connect(DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 // Connection Events
 mongoose.connection
-.on("open", () => console.log("You are connected to Mongo"))
-.on("close", () => console.log("You are disconnected from Mongo"))
-.on("error", (error) => console.log(error))
+  .on("open", () => console.log("You are connected to Mongo"))
+  .on("close", () => console.log("You are disconnected from Mongo"))
+  .on("error", (error) => console.log(error));
+
+//////////////////////////////
+// Models
+//////////////////////////////
+// the people schema
+const PeopleSchema = new mongoose.Schema(
+  {
+    name: String,
+    image: String,
+    title: String,
+  },
+  { timestamps: true }
+);
+
+const People = mongoose.model("People", PeopleSchema);
+
+/////////////////////////////////
+//Middleware
+//////////////////////////////////
+app.use(cors()); // prevent cors errors, opens up access for frontend
+app.use(morgan("dev")); //logging
+app.use(express.json()); // parse json bodies
 
 ////////////////////////////////
 // Routes
 ////////////////////////////////
 // create a test route
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
 // People index route
 // get request to /people, returns all people as json
 app.get("/people", async (req, res) => {
-    try {
-      // send all people
-      res.json(await People.find({}));
-    } catch (error) {
-      res.status(400).json({ error });
-    }
-  });
+  try {
+    // send all people
+    res.json(await People.find({}));
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
 
-  // People create route
+// People create route
 // post request to /people, uses request body to make new people
 app.post("/people", async (req, res) => {
-    try {
-      // screate a new person
-      res.json(await People.create(req.body));
-    } catch (error) {
-      res.status(400).json({ error });
-    }
-  });
+  try {
+    // screate a new person
+    res.json(await People.create(req.body));
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
 
-  // People update  route
+// People update  route
 // put request /people/:id, updates person based on id with request body
 app.put("/people/:id", async (req, res) => {
     try {
@@ -104,4 +107,6 @@ app.delete("/people/:id", async (req, res) => {
 /////////////////////////////////
 // Server Listener
 /////////////////////////////////
-app.listen(PORT, () => {console.log(`listening on PORT ${PORT}`)})
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
